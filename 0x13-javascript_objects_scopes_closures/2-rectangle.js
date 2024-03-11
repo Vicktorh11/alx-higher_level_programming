eee1370c74ddbe8aef4c87aefd0f04862f6533b5
Vicktorh11 <prinzvee11@gmail.com>
2024-03-11 09:36:34 -0400

    rectangle

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+// Checks for Rectangle parameters
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && typeof h === 'number' && w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+};
