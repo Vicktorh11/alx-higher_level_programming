9eb5a7314dbf3cc2977b8e3c8cc50a327a92d08c
Vicktorh11 <prinzvee11@gmail.com>
2024-03-11 08:15:53 -0400

    initilizing const

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// A Rectangle class with width and height attributes.
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
